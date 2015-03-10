16640e42cd9e3c2112d96926d25d6eb6d72e3b97
Jonathan Sokoll <sokoll.jc@gmail.com>
2015-03-10 17:13:00 -0400

    Added function to pull in a publication's abstract

--- SSRN.py
+++ SSRN.py
@@ -10,19 +10,44 @@ def ssrn_author_publications_search(name):
 	for result in soup.find_all(valign="top", width="96%"):
 		
 		#Publication Title
-		title = result.strong.string
+		title_raw = result.strong.string
+		title_utf8 = title_raw.encode('utf-8')
+		
+		#Extracts Unique identifier for publication
+		for title_tag in result.find_all(target="_top"):
+			pub_url = title_tag['href']
+			print(pub_url)
+			pub_id = pub_url[23:]
+			print(pub_id)
+			#Downloads the abstract from another page
+			abstract = publication_abstract(pub_id)
+			print(abstract)
 		
 		#Publication (ie Journal) information
 		if result.i is not None:
 			pub_info = result.i.string
 		elif result.i is None:
 			pub_info = ""
-		print(pub_info.decode('utf-8'))
+		#print(pub_info.decode('utf-8'))
 
 		#Authors
 		for authors in result.find_all("a"):
+			pair = {}
 			author = authors.get_text()
-			print(author)
-		
-		
+			author_utf8 = author.encode('utf-8')
+			author_id = authors['target']
+			pair[author_utf8]= author_id
+			#print(pair)
+
+def publication_abstract(publication_id):
+	parameters = {'abstract_id':publication_id}
+	r = requests.get('http://papers.ssrn.com/sol3/papers.cfm?', params=parameters)
+	
+	soup = BeautifulSoup(r.content)
+	
+	abstract_raw = soup.find(id="abstract").get_text()
+	abstract_utf8 = abstract_raw.encode('utf-8')
+	
+	return abstract_utf8
+
 ssrn_author_publications_search("John Smith")
